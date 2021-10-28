28c54f883a34631caba001e036b7eae2296eb23a
onifs10 <onifadebolu64@gmail.com>
2021-10-28 23:09:51 +0100

    done with day 001

--- pages/001.tsx
+++ pages/001.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
-import { MouseEvent, useEffect, useMemo, useRef } from "react";
+import { MouseEvent, useEffect, useMemo, useRef, useState } from "react";
 import Styles from "../styles/001.module.scss";
+import { Button } from "@blueprintjs/core";
 import Matter, { Runner } from "matter-js";
 
 const {
@@ -16,6 +17,10 @@ const {
 const Day001: NextPage = () => {
   //====  states =====//
   const divRef = useRef<HTMLDivElement | null>(null);
+  const renderRef = useRef<Matter.Render>();
+  const [running, setRuning] = useState<boolean>(true);
+  const runnerRef = useRef<Matter.Runner>(Runner.create());
+  const [reset, setReset] = useState<boolean>(true);
   // actions to be called in this Componen
   const actions = useMemo(
     () => ({
@@ -30,22 +35,17 @@ const Day001: NextPage = () => {
       strokeStyle: "green",
       lineWidth: 2,
     }),
-    []
+    [reset]
   );
-  // create and engine
-  const engine = Engine.create();
-
-  actions.add = (event) => {
-    let { x, y } = event.source.mouse.absolute;
-    const boxA = Bodies.rectangle(x, y, 30, 30, { render: wireframe });
-    World.add(engine.world, [boxA]);
-  };
 
   useEffect(() => {
     // create a renderer
     if (divRef.current) {
+      // create and engine
+      const engine = Engine.create();
+
       divRef.current.innerHTML = "";
-      const render = Render.create({
+      renderRef.current = Render.create({
         element: divRef.current,
         engine: engine,
         options: {
@@ -63,36 +63,75 @@ const Day001: NextPage = () => {
 
       World.add(engine.world, [ground]);
       // run the engine
-      Runner.run(engine);
+      Runner.start(runnerRef.current, engine);
 
       // run the renderer
-      Render.run(render);
+      Render.run(renderRef.current);
 
       //create a mouse for the div
-      var mouse = Mouse.create(render.element);
+      var mouse = Mouse.create(renderRef.current.element);
 
       //create a mouseConstarint for user interaction
       var mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
       });
 
-      //   let { x, y } = event.source.mouse.absolute;
       const boxA = Bodies.rectangle(200, 200, 30, 30, { render: wireframe });
       World.add(engine.world, [boxA]);
+
+      actions.add = (event) => {
+        let { x, y } = event.source.mouse.absolute;
+        const boxA = Bodies.rectangle(x, y, 30, 30, { render: wireframe });
+        World.add(engine.world, [boxA]);
+      };
 
       Composite.add(engine.world, mouseConstraint);
 
       //register mouse down event to the mouse constraint
       Events.on(mouseConstraint, "mousedown", actions.add);
 
-      render.mouse = mouse;
+      renderRef.current.mouse = mouse;
+
       return () => {
         Events.off(mouseConstraint, "mousedown", actions.add);
       };
     }
-  }, [divRef, engine, wireframe, actions]);
+  }, [divRef, wireframe, actions]);
 
-  return <div ref={divRef} className={Styles.box}></div>;
+  const pauseEngine = () => {
+    if (runnerRef.current) {
+      runnerRef.current.enabled = !running;
+      setRuning((value) => !value);
+    }
+  };
+
+  const resetEngine = () => {
+    runnerRef.current.enabled = true;
+    setRuning(true);
+    setReset((value) => !value);
+  };
+
+  return (
+    <div className={Styles.pageWrapper}>
+      <div ref={divRef} className={Styles.box} />
+      <div className={Styles.tools}>
+        <Button
+          icon={running ? "pause" : "play"}
+          intent="primary"
+          text={running ? "Pause" : "Play"}
+          className={Styles.toolButton}
+          onClick={pauseEngine}
+        />
+        <Button
+          icon="reset"
+          intent="danger"
+          text={"Reset"}
+          className={Styles.toolButton}
+          onClick={resetEngine}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Day001;
